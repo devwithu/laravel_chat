df95b346ba5dc0dec09bdb7d523dd94f464de873
devwithu <devwithu@naver.com>
2019-11-04 21:38:58 +0900

    init

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <style>
+        .list-group{
+            overflow-y: scroll;
+            height: 200px;
+        }
+    </style>
+
+</head>
+<body>
+    <div class="container">
+        <div class="row" id="app">
+            <div class="offset-4 col-4"></div>
+                <li class="list-group-item active">Chat Room</li>
+                <ul class="list-group">
+
+                    <message v-for="value in chat.message">@{{ value }}</message>
+
+                </ul>
+                <input type="text" class="form-control" placeholder="Type your message here..."
+                       v-model="message" @keyup.enter='send'>
+            </div>
+        </div>
+    </div>
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
